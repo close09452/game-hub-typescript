230a88b4ac3ae5168591da26089a3447d3214763
Manhattan_Chen <close09452@yahoo.com.tw>
2023-06-08 09:47:39 +0800

    displaying critic score

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,22 +1,26 @@
-
 import { Game } from "../hooks/useGames";
-import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react";
+import { Card, CardBody, HStack, Heading, Image, Text } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
+import CriticScore from "./CriticScore";
 
 interface Props {
   game: Game;
 }
 
 const GameCard = ({ game }: Props) => {
-
   return (
     <Card borderRadius={10} overflow="hidden">
       <Image src={game.background_image} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
-        <PlatformIconList
-          platform={game.parent_platforms.map((platform) => platform.platform)}
-        />
+        <HStack justifyContent="space-between">
+          <PlatformIconList
+            platform={game.parent_platforms.map(
+              (platform) => platform.platform
+            )}
+          />
+          <CriticScore score={game.metacritic} />
+        </HStack>
       </CardBody>
     </Card>
   );
