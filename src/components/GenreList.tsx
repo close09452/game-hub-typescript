591cc28fb41bb87c0ed92dc4808ceef28e877970
Manhattan_Chen <close09452@yahoo.com.tw>
2023-06-08 10:36:57 +0800

    fetch genres

---
+++ src/components/GenreList.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import useGenre from "../hooks/useGenre";
+
+const GenreList = () => {
+  const { genre } = useGenre();
+  return (
+    <ul>
+      {genre.map((g) => (
+        <li key={g.id}>{g.name}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default GenreList;
