feb777b06a2b79e59856a09a530edd23257a7575
Manhattan_Chen <close09452@yahoo.com.tw>
2023-06-09 14:06:32 +0800

    ship genre to the app

--- src/hooks/useGenre.ts
+++ src/hooks/useGenre.ts
@@ -1,14 +1,12 @@
-import useData from "./useData";
+import genres from "../data/genres";
 
 export interface Genre {
   id: number;
   name: string;
   slug: string;
-  image_background:string;
+  image_background: string;
 }
 
-const useGenre = () => {
-  return useData<Genre>("/genres");
-};
+const useGenre = () => ({ data: genres, isLoading: false, error: null });
 
 export default useGenre;
