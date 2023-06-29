93cb71e67fc02f18aa1c6823bf0bde52d33b4efe
Manhattan_Chen <close09452@yahoo.com.tw>
2023-06-29 14:12:40 +0800

    handle error page

--- src/pages/routes.tsx
+++ src/pages/routes.tsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import GameDetailPage from "../components/GameDetailPage";
 import Layout from "./Layout";
 import HomePage from "./HomePage";
+import ErrorPage from "./ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       { index: true, element: <HomePage /> },
       {
