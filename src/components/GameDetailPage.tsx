ce8faf6b5b5b566e99bb6f2c547e4f91c3bbf2f6
Manhattan_Chen <close09452@yahoo.com.tw>
2023-06-30 17:18:06 +0800

    build game attributes

--- src/components/GameDetailPage.tsx
+++ src/components/GameDetailPage.tsx
@@ -3,6 +3,8 @@ import { useParams } from "react-router-dom";
 import { Heading, Spinner } from "@chakra-ui/react";
 import ExpandableText from "./ExpandableText";
 
+import GameAttributes from "./GameAttributes";
+
 const GameDetailPage = () => {
   const { slug } = useParams();
   const { data: game, error, isLoading } = useGame(slug!);
@@ -11,7 +13,8 @@ const GameDetailPage = () => {
   return (
     <>
       <Heading>{game.name}</Heading>
-      <ExpandableText>{game.description_raw }</ExpandableText>
+      <ExpandableText>{game.description_raw}</ExpandableText>
+      <GameAttributes game={game} />
     </>
   );
 };
